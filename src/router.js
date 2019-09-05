610b9abc5bc7cdfa47bc90e07b38b26e1ebf004b
yangjingsheng <673630886@qq.com>
2019-09-05 23:22:20 +0800

    提交第一个页面

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import BookIn from './components/BookIn.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/bookin',
+      name: 'bookin',
+      component: BookIn
     },
     {
       path: '/about',
--- src/store.js
+++ src/store.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
   },
   mutations: {
-
   },
   actions: {
-
+    submit (values) {
+      axios
+        .post('http://localhost:8080/bookin', {
+          bookNo: values.bookNo,
+          recipient: values.recipient,
+          telNum: values.telNum,
+          status: 'weiqianshou',
+          reservationTime: '',
+          bookWeight: values.bookWeight
+        })
+    }
   }
 })
